d92ecd2dab10f6ed69ec82861188d8c623cc9d4a
tomato <tomato@yandex-team.ru>
2019-09-21 16:20:24 +0300

    Sync expected values in tests with the paper (https://dl.acm.org/citation.cfm?id=3055547)

--- tests/test_truth_inference.py
+++ tests/test_truth_inference.py
@@ -1,11 +1,9 @@
 import unittest
-from typing import Iterable
 
-from crowd_inference.methods.majority_vote import MajorityVote
 from crowd_inference.methods.dawid_skene import DawidSkene
-from crowd_inference.model.estimation import Estimation
-from crowd_inference.truth_inference import TruthInference
-from .data_provider import SimpleGeneratedDataProvider, RelDataProvider, AdultsDataProvider
+from crowd_inference.methods.majority_vote import MajorityVote
+from crowd_inference.truth_inference import NoFeaturesInference
+from .data_provider import SimpleGeneratedDataProvider, RelDataProvider, AdultsDataProvider, DataProvider
 
 
 class TestTruthInference(unittest.TestCase):
@@ -19,43 +17,35 @@ class TestTruthInference(unittest.TestCase):
                                               './resources/datasets/adults/gold.txt')
 
     def test_majority_vote(self):
-        inference = MajorityVote()
-
-        inference.fit(self._simple_data.labels())
-        self.assertAlmostEqual(self._accuracy(self._simple_data.gold(), inference), 0.33, places=2)
-
-        inference.fit(self._adults_data.labels())
-        self.assertAlmostEqual(self._accuracy(self._adults_data.gold(), inference), 0.76, places=2)
-
-        inference.fit(self._rel_data.labels())
-        self.assertAlmostEqual(self._accuracy(self._rel_data.gold(), inference), 0.53, places=2)
+        mv = MajorityVote()
+        self._assert_accuracy(self._simple_data, mv, 0.33)
+        self._assert_accuracy(self._adults_data, mv, 0.76)
+        self._assert_accuracy(self._rel_data, mv, 0.53)
 
     def test_dawid_skene(self):
-        inference = DawidSkene()
+        ds = DawidSkene()
+        self._assert_accuracy(self._simple_data, ds, 0.33)
+        self._assert_accuracy(self._adults_data, ds, 0.76)
+        self._assert_accuracy(self._rel_data, ds, 0.61)
 
-        # inference.fit(self._simple_data.labels())
-        # print(f"Simple accuracy: {self._accuracy(self._simple_data.gold(), inference)}")
-
-        inference.fit(self._adults_data.labels())
-        print(f"Adult accuracy: {self._accuracy(self._adults_data.gold(), inference)}")
-
-        inference.fit(self._rel_data.labels())
-        print(f"Rel accuracy: {self._accuracy(self._rel_data.gold(), inference)}")
-
-    @staticmethod
-    def _accuracy(gold: Iterable[Estimation], inference: TruthInference) -> float:
+    def _assert_accuracy(self, provider: DataProvider, inference: NoFeaturesInference,
+                         expected_accuracy: float) -> None:
         accepted = 0
         all_points = 0
         estimates = {}
+
+        inference.fit(provider.labels())
         for estimate in inference.estimate():
             estimates[estimate.task] = estimate.value
-        for point in gold:
+        for point in provider.gold():
             if point.task in estimates:
                 estimate = estimates[point.task]
                 all_points += 1
                 if point.value == estimate:
                     accepted += 1
-        return accepted / all_points
+
+        accuracy = accepted / all_points
+        self.assertAlmostEqual(expected_accuracy, accuracy, places=2)
 
 
 if __name__ == '__main__':
